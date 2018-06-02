87827957e21adccd78d5eb7ba22e301d21ca1fa2
gurpreetverma <39779004+gurpreetverma@users.noreply.github.com>
2018-06-02 17:17:50 -0400

    Add files via upload

--- Person.java
+++ Person.java
@@ -1,73 +1,95 @@
-
-import static java.lang.System.in;
-import java.util.Scanner;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package day4;
 
 /**
  *
  * @author macstudent
  */
-public class Person {
+import java.util.Scanner;
+
+/**
+ *
+ * @author macstudent
+ */
+public class Person 
+{
     String name;
     String address;
     int age;
-    Scanner in= new Scanner(System.in);
+    Scanner sc=new Scanner(System.in);
+    
     //default constructor
-    Person(){
-        name ="unknown";
-        address ="unknown";
+    Person()
+    {
+        name= "unknown";
+        address="unknown";
         age=1;
     }
-    Person(Person anotherPerson){
-        this.name = anotherPerson.name;
+    
+    //parametrized constructor
+    Person(String name,String address,Integer age)
+    {
+      this.name=name;
+      this.address=address;
+      this.age=age;
+    }
+
+    Person(Person anotherPerson) {
+        this.name=anotherPerson.name;
         this.address=anotherPerson.address;
         this.age=anotherPerson.age;
     }
-    Person(String name,String address,int age){
-        this.name = name;
-        this.address=address;
-        this.age=age;
-    }
-   
-    void setName(){
-        
-        System.out.println("Enter Name:");
-        name = in.nextLine();
-    }
-    String getName(){
-        return name;
     
-}
-    void setAddress(){
-        System.out.println("Enter Address:");
-        address =in.nextLine();
-    }
-    String getAddress(){
-        return address;
-    }
-    void setAge(){
-        System.out.println("Enter Age:");
-        age =Integer.parseInt(in.nextLine());
-    }
-    int getAge(){
-        return age;
-    }
-    void setData()
+    void setName()
     {
-        setName();
-        setAge();
-        setAddress();
-    }
-    @Override
-    public String toString(){
-        String data ="Name:" +name+"\n Address:"+address+"\n Age:"+age;
-        return data;
         
+        System.out.println("Enter name : ");
+        this.name = sc.nextLine();
     }
+    
+    String getName()
+    {
+      return this.name;  
     }
-
+     
+   void setAddress()
+   {
+       System.out.println("Enter address:");
+       this.address = sc.nextLine();
+   }
+   
+   String getaddress()
+   {
+       return this.address;
+      
+   }
+   
+   void setAge()
+   {
+       System.out.println("Enter age : ");
+       age = Integer.parseInt(sc.nextLine());
+   }
+   
+   int getAge()
+   {
+      return age; 
+   }
+    
+   void setData()
+   {
+       setName();
+       setAddress();
+       setAge();
+   }
+   
+    @Override
+   public String toString()
+    {
+        String data = "Name : " + name + "\n Address : " + address + "\n Age : " + age;
+         return data;     
+    }
+}
